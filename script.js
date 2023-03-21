2169739a4ebf034aa84302108584d9ac4641ab6a
wbisoles1214 <wbisoles@gmail.com>
2023-03-21 20:55:54 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// String type methods in JavaScript
+// slice method
+let text = "Apple, Banana, Kiwi";
+let part = text.slice(7, 13);
+
+console.log(part);
+
+// substring method
+let str = "Apple, Banana, Kiwi";
+let fruit = str.substring(7, 13);4
+
+console.log(fruit);
+
+// replace method
+let newText = "Please visit Microsoft";
+let updatedText = newText.replace("Microsoft", "KodeGo");
+
+console.log(updatedText);
+
+
+
+
+
+
+
+
+
+
+
